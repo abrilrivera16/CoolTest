eee2cf62fbe89116258f621f4f892c799209624e
Abril <Abril@ABRIL_DIEGO>
2024-07-27 08:30:23 -0600

    Primer Commit

---
+++ Julio24/src/main/java/JavaTest/Selenium_test.java
@@ -0,0 +1,15 @@
+package JavaTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Selenium_test {
+
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+
+	}
+
+}
